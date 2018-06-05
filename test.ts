efb462b6f2e963f3988fd6eb44f4078b332ffd59
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-06-05 10:47:58 +0200

    test: Wrap top-level tests in a `describe()` block

--- test.ts
+++ test.ts
@@ -14,13 +14,15 @@ const DATA_PATH = `${__dirname}/data`;
 
 const files = fs.readdirSync(`${DATA_PATH}`).filter(filename => (/\.cup$/).test(filename));
 
-for (let filename of files) {
-  test(filename, () => {
-    let content = fs.readFileSync(`${DATA_PATH}/${filename}`, 'utf8');
-    let result = parse(content);
-    expect(result).toMatchSnapshot(filename);
-  });
-}
+describe('parse()', () => {
+  for (let filename of files) {
+    test(filename, () => {
+      let content = fs.readFileSync(`${DATA_PATH}/${filename}`, 'utf8');
+      let result = parse(content);
+      expect(result).toMatchSnapshot(filename);
+    });
+  }
+});
 
 describe('parseHDistance()', () => {
   const TESTS = [
